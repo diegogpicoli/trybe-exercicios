a8b1eb0e1e704669fc265e5264fb2ecd2643c362
Diego Picoli <dgpicoli@gmail.com>
2022-08-22 11:44:33 -0300

    Exercicios dia 14

---
+++ front-end/bloco-14-testes-automatizados-com-react-testing-library/exercicio/teste/service.js
@@ -0,0 +1,8 @@
+// Gera  número aleatório entre 0 e 100
+const aleatoryNumber = () => {
+    return Math.floor(Math.random() * 100)
+}
+
+console.log(aleatoryNumber())
+
+export default aleatoryNumber;
